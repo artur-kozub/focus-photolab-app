c515577032b288fec31dea06a57aa65de363878d
Arthur Kozub <kozub0246@gmail.com>
2024-07-04 12:29:29 +0300

    fix: combined all files server and bot to avoid conflicts

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const TelegramApi = require('node-telegram-bot-api')
+const express = require('express');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const bot = new TelegramApi(process.env.BOT_TOKEN, { polling: true })
-const webUrl = process.env.WEB_APP_URL
+const webUrl = process.env.WEB_APP_URL;
+
+const app = express();
+const PORT = process.env.EXPRESS_PORT | 7000
 
 bot.setMyCommands([
     { command: '/start', description: 'Запуск бота' }
@@ -21,4 +25,12 @@ bot.onText(/\/start/, async (msg) => {
     })
 })
 
-module.exports = bot;
+app.get('/', (req, res) => {
+    res.send('Bot is running...')
+})
+
+app.listen(PORT, () => {
+    console.log('Server for bot is started...')
+})
+
+console.log('Bot and server initialized successfully');
