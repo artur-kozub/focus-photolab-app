3f2c36a722b56e4b4e67514ef4ec1590e0660a35
Arthur Kozub <kozub0246@gmail.com>
2024-07-04 11:34:27 +0300

    fix: hide webUrl to .env | refactor bot.on() listener | add command /start

--- src/bot.js
+++ src/bot.js
@@ -1,19 +1,22 @@
 const TelegramApi = require('node-telegram-bot-api')
 require('dotenv').config()
 const bot = new TelegramApi(process.env.BOT_TOKEN, { polling: true })
-const webUrl = 'https://photostudio-booking.netlify.app/'
+const webUrl = process.env.WEB_APP_URL
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id
-    const text = msg.text
 
-    if(text === '/start'){
+    bot.setMyCommands([
+        { command: '/start', description: 'Запуск бота' }
+    ])
+
+    bot.onText(/\/start/, async () => {
         bot.sendMessage(chatId, 'Кнопка щоб забронювати студію нижче:', {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: 'Забронювати студію', web_app: {url: webUrl}}]
+                    [{ text: 'Забронювати студію', web_app: { url: webUrl } }]
                 ]
             }
         })
-    }
+    })
 })
